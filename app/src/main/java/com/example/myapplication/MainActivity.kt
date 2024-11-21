287c41487e009b128f6e6a845ed5942cde23d5ba
Bakhodir <solievbakhodir.05@gmail.com>
2024-11-21 15:49:44 +0100

    just recently merged my second pull request

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,5 +3,15 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity()  {
+/*Create new Account
+    Correct Input values
+        Password (8 charachteers/digits)
+        Email Format
+    Email already used - Error
+    Successful registration (new user is present in CredManager)
+    Case Insensitive in email
 
+    Apply it to SignUpDetailsActivity
+    After successful registration, go to CreateAccount activity
+    */
 }
